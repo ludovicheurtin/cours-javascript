42e4c284dcf2d8a6b177f5ca23b5579a25dd1788
portable_ludo <rigoludo@gmail.com>
2019-01-25 11:51:04 +0100

    verif mail V2

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('btn').addEventListener("click", toggle);
-            
+
 document.getElementById('retour').addEventListener("click", toggle);
- 
+
 function toggle(popin) {
     node = document.getElementById("popin");
     if (node.style.visibility=="visible") {
@@ -9,7 +9,6 @@ function toggle(popin) {
     } else {
         node.style.visibility = "visible";
     }
-
     node = document.getElementById("anticlick");
     if (node.style.visibility=="visible") {
         node.style.visibility = "hidden";
@@ -18,12 +17,38 @@ function toggle(popin) {
     }
 }
 
-function valider(mail){
-    mail = document.getElementById(mail);
-    if (mail.indexOf("@")!= -1 && mail.indexOf('.')!=-1) {
-        document.location.href="redirection.html";
-    }
-    else{
-       document.write("L'email est incorrecte");
-    }
+function surligne(champ, erreur)
+{
+   if(erreur)
+      champ.style.backgroundColor = "#fba";
+   else
+      champ.style.backgroundColor = "";
+}
+
+function verifMail(champ)
+{
+   var regex = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+   if(!regex.test(champ.value))
+   {
+      surligne(champ, true);
+      return false;
+   }
+   else
+   {
+      surligne(champ, false);
+      return true;
+   }
+}
+
+function verifForm(f)
+{
+   var mailOk = verifMail(f.email);
+   
+   if(mailOk)
+      return true;
+   else
+   {
+      alert("Veuillez remplir correctement tous les champs");
+      return false;
+   }
 }
