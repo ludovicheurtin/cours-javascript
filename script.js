81f0183686f27255e8367d2fde4636e60f21ada1
portable_ludo <rigoludo@gmail.com>
2019-01-25 09:40:51 +0100

    Ajout d'un script de verif mail

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 document.getElementById('btn').addEventListener("click", toggle);
             
-            document.getElementById('retour').addEventListener("click", toggle);
+document.getElementById('retour').addEventListener("click", toggle);
  
-            function toggle(popin) {
-                node = document.getElementById("popin");
-                if (node.style.visibility=="visible") {
-                    node.style.visibility = "hidden";
-                } else {
-                    node.style.visibility = "visible";
-                }
-                
-                node = document.getElementById("anticlick");
-                if (node.style.visibility=="visible") {
-                    node.style.visibility = "hidden";
-                } else {
-                    node.style.visibility = "visible";
-                }
-            }
+function toggle(popin) {
+    node = document.getElementById("popin");
+    if (node.style.visibility=="visible") {
+        node.style.visibility = "hidden";
+    } else {
+        node.style.visibility = "visible";
+    }
+
+    node = document.getElementById("anticlick");
+    if (node.style.visibility=="visible") {
+        node.style.visibility = "hidden";
+    } else {
+        node.style.visibility = "visible";
+    }
+}
+
+function valider(mail){
+    mail = document.getElementById(mail);
+    if (mail.indexOf("@")!= -1 && mail.indexOf('.')!=-1) {
+        document.location.href="redirection.html";
+    }
+    else{
+       document.write("L'email est incorrecte");
+    }
+}
